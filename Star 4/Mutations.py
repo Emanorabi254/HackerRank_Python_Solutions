701acad4858b3a72fe643b205bfca5576ca4c8ae
Eman Orabi <159061835+Emanorabi254@users.noreply.github.com>
2025-12-12 20:29:25 +0200

    Add Mutations Problem Solution

---
+++ Star 4/Mutations.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    
+    List = list(string)
+    List[position]= character
+    string = "".join(List)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
