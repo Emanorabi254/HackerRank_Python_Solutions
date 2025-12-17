8582b81be9ae8539e218ce53e0b7e39fab31e5bc
Eman Orabi <159061835+Emanorabi254@users.noreply.github.com>
2025-12-17 10:50:37 +0200

    Add the solution of the problem of The_Minion_Game

---
+++ Star 5/The_Minion_Game.py
@@ -0,0 +1,22 @@
+def minion_game(string):
+    vowels ="AEIOU"
+    kevin = 0
+    stuart = 0
+    
+    n= len(string)
+    for i in range(n):
+        if string[i] in vowels:
+            kevin += n-i
+        else:
+            stuart += n-i 
+    if kevin > stuart:
+        print(f"Kevin {kevin}")
+    elif kevin < stuart :
+        print(f"Stuart {stuart}")               
+    else:
+        print("Draw")    
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
